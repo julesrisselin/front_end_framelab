34f82b71c222fb200f5a9bda0c04b8b06ecf6beb
Jules <julesrisselin@gmail.com>
2026-01-11 17:46:03 +0100

    formulaire de connexion correct

--- src/Views/login.vue
+++ src/Views/login.vue
@@ -1,15 +1,24 @@
-<script setup> 
+<script setup>
+    import { useRoute, useRouter } from 'vue-router'
+
+    const router = useRouter();
+
+    async function goToSignIn() {
+        router.push('/signin');
+    }
 </script>
 
 <template>
-<form action="http://localhost:3000/auth/login" enctype="multipart/form-data" method="post">
-            <div class="form-group">
-                <input type="text" class="email" placeholder="email" name="email">
-                <input type="text" class="password" placeholder="password" name="password">
-            </div>
-        </form>
+    <form action="http://localhost:3000/api/auth/login" method="post">
+        <div class="form-group">
+            <input type="text" class="email" placeholder="email" name="email">
+            <input type="text" class="password" placeholder="password" name="password">
+            <input type="submit" value="Envoyer" class="btn btn-default"></input>
+        </div>
+    </form>
+    <br>
+
+    <button @click=goToSignIn()> Ici pour créer un compte </button>
 </template>
 
-<style scoped>
-    
-</style>
+<style scoped></style>
--- src/Views/signin.vue
+++ src/Views/signin.vue
@@ -2,12 +2,13 @@
 </script>
 
 <template>
-<form action="http://localhost:3000/users" enctype="multipart/form-data" method="post">
+<form action="http://localhost:3000/api/users"  method="post">
             <div class="form-group">
                 <input type="text" class="email" placeholder="email" name="email">
                 <input type="text" class="password" placeholder="password" name="password">
                 <input type="text" class="name" placeholder="name" name="name">
                 <input type="text" class="firstname" placeholder="prénom" name="firstname">
+                <input type="submit" value="Envoyer" class="btn btn-default"></input>
             </div>
         </form>
 </template>
