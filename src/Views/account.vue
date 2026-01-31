bbc068561f0df01d7e8179d39b92437bf0b40c70
Jules <julesrisselin@gmail.com>
2026-01-31 22:55:05 +0100

    Affichage infos utilisateurs

--- src/Views/account.vue
+++ src/Views/account.vue
@@ -1,8 +1,98 @@
 <script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const authentification = ref(false);
+const userInfos = ref([]);
+
+async function Account() {
+    const respAccount = await fetch("http://localhost:3000/api/users/me",{
+        credentials : "include"
+    })
+    
+    if (respAccount.status === 200){
+        authentification.value = true;
+    } else {
+        authentification.value = false;
+    }
+
+    const dataUser = await respAccount.json();
+    userInfos.value = dataUser;
+}
+
+    async function goToCurrentChallenge(){
+        router.push('/currentChallenge');
+    }
+
+    async function goToAccueil(){
+        router.push('/');
+    }
+
+    async function goToCurrentParticipations(){
+        router.push('/currentParticipations');
+    }
+
+    async function goToParticipations(){
+        router.push('/participations');
+    }
+
+    async function goToLogin(){
+        router.push('/login');
+    }
+
+    async function goToAccount(){
+        router.push('/account');
+    }
+
+
+
+
+Account();
 
 </script>
 
 <template>
+    <header>
+        <nav id="navbar">
+            <ul>
+                <li>
+                    <button @click= goToAccueil()> Accueil </button>
+                </li>
+                <li>
+                    <button @click= goToCurrentChallenge()> Challenge </button>
+                </li>
+                <li>
+                    <button @click= goToCurrentParticipations()> Participations de la semaine </button>
+                </li>
+                <li>
+                    <button @click= goToParticipations()> Toutes les participations </button>
+                </li>
+                <li v-if="!authentification">
+                    <button @click= goToLogin() id="Account">  Connexion > </button>
+                </li>
+                <li v-else>
+                    <button @click= goToAccount() id="Account"> Mon compte > </button>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <li> <h1>Informations de votre compte </h1>
+        <ul>
+            Email : {{ userInfos.email }}
+        </ul>
+        <ul>
+            Nom : {{ userInfos.name }}
+        </ul>
+        <ul>
+            Prénom : {{ userInfos.firstname }}
+        </ul>
+        <ul>
+            Date d'inscription : {{ userInfos.date_inscription }}
+        </ul>
+        
+    </li>
 
 </template>
 
