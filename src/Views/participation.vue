24203406be342cc8ae0218878ac568c4258cd554
Jules <julesrisselin@gmail.com>
2026-01-31 22:55:42 +0100

    Envoi votes et commentaires

--- src/Views/participation.vue
+++ src/Views/participation.vue
@@ -9,8 +9,14 @@ const participation = ref([]);
 const user = ref([]);
 const comments = ref([]);
 const votes = ref([]);
+const authentification = ref(false);
+const userInfos = ref([]);
+const commentaires = ref("");
 
-console.log(route.params.id)
+const note_creativity = ref(0);
+const note_on_theme = ref(0);
+const note_technique = ref(0);
+
 
 const paramsPart = new URLSearchParams();
 paramsPart.append("id_participation", id_participation);
@@ -38,8 +44,52 @@ async function getData() {
 
     const respVotes = await fetch(`http://localhost:3000/api/votes/:id${paramsComVotes}`)
     const dataVotes = await respVotes.json();
-    //console.log(dataVotes);
     votes.value = dataVotes;
+
+    const respAccount = await fetch("http://localhost:3000/api/users/me", {
+        credentials: "include"
+    })
+
+    if (respAccount.status === 200) {
+        authentification.value = true;
+    } else {
+        authentification.value = false;
+    }
+
+    const userData = await respAccount.json();
+    userInfos.value = userData;
+}
+
+async function sendComments() {
+    const response = await fetch("http://localhost:3000/api/comments", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            id_participations: id_participation,
+            user_id: userInfos.value.id,
+            content: commentaires.value
+        }),
+    });
+}
+
+async function sendVotes() {
+    const response = await fetch("http://localhost:3000/api/votes", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            id_participations: id_participation,
+            user_id: userInfos.value.id,
+            note_creativity: note_creativity.value,
+            note_on_theme: note_on_theme.value,
+            note_technique: note_technique.value,
+        }),
+    });
 }
 
 
@@ -63,6 +113,10 @@ async function goToLogin() {
     router.push('/login');
 }
 
+async function goToAccount() {
+    router.push('/account');
+}
+
 getData();
 </script>
 
@@ -82,8 +136,11 @@ getData();
                 <li>
                     <button @click=goToAllParticipations()> Toutes les participations </button>
                 </li>
-                <li>
-                    <button @click=goToLogin()> Connexion > </button>
+                <li v-if="!authentification">
+                    <button @click=goToLogin() id="Account"> Connexion > </button>
+                </li>
+                <li v-else>
+                    <button @click=goToAccount() id="Account"> Mon compte > </button>
                 </li>
             </ul>
         </nav>
@@ -99,39 +156,38 @@ getData();
         <h3> Pour le challenge N°{{ participation.data.id_challenge }} </h3>
     </div>
 
-    <form action="http://localhost:3000/api/votes" method="post">
-        <div class="form-group">
-            <input type="number" max="5" class="vote" placeholder="note de créativité" name="note_creativity">
-            <input type="number" max="5" class="vote" placeholder="note de technique" name="note_technique">
-            <input type="number" max="5" class="vote" placeholder="note respect du thème" name="note_on_theme">
-            <input type="submit" value="Envoyer" class="btn btn-default">
-        </div>
-    </form>
+    <input type="number" v-model=note_creativity max="5" class="vote" placeholder="note de créativité"
+        name="note_creativity">
+    <input type="number" v-model=note_on_theme max="5" class="vote" placeholder="note de technique"
+        name="note_technique">
+    <input type="number" v-model=note_technique max="5" class="vote" placeholder="note respect du thème"
+        name="note_on_theme">
+    <button @click=sendVotes() id=""> Envoyer </button>
+
+
     <br>
-    <form action="http://localhost:3000/api/comments" method="post">
-        <div class="form-group">
-            <input type="text" class="comments" placeholder="Entrez un commentaires" name="comments">
-            <input type="submit" value="Envoyer" class="btn btn-default">
-        </div>
-    </form>
+
+    <input type="text" v-model=commentaires class="comments" placeholder="Entrez un commentaires" name="comments">
+    <button @click=sendComments() id=""> Envoyer </button>
+
     <h4> Votes :</h4>
     <li v-for="(votes) in votes.data">
         <h5> Compte N° {{ votes.user_id }} </h5>
-    <ul>
-        Note de créativité : {{ votes.note_creativity }}
-    </ul>
-    <ul>
-        Note respect du thème : {{ votes.note_on_theme }}
-    </ul>
-    <ul>
-        Note de technique : {{ votes.note_technique }}
-    </ul>
+        <ul>
+            Note de créativité : {{ votes.note_creativity }}
+        </ul>
+        <ul>
+            Note respect du thème : {{ votes.note_on_theme }}
+        </ul>
+        <ul>
+            Note de technique : {{ votes.note_technique }}
+        </ul>
     </li>
 
 
     <h4> Commentaire :</h4>
     <li v-for="(comments) in comments.data">
-        <h5> Compte N° {{  comments.user_id }} </h5>
+        <h5> Compte N° {{ comments.user_id }} </h5>
         {{ comments.content }}
     </li>
 
